f80c14d90c66e349efbf7aefa031cf2a93f80b98
anurag sharma <anuragsharma2092003@gmail.com>
2025-11-17 09:30:30 +0530

    Create eventBasedCounter.js

---
+++ eventBasedCounter.js
@@ -0,0 +1,26 @@
+let count = 0;
+
+function increment() {
+    count++;
+    console.log("Count:", count);
+
+    function inner() {
+        console.log("Inner Count:", count);
+    }
+    inner();
+}
+
+function decrement() {
+    count--;
+    console.log("Count:", count);
+
+    function inner() {
+        console.log("Inner Count:", count);
+    }
+    inner();
+}
+
+increment();
+increment();
+decrement();
+increment();
