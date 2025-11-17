42c9fffdb36a03bd8c56bb71feaf1cc6f14f9c7d
anurag sharma <anuragsharma2092003@gmail.com>
2025-11-17 09:27:37 +0530

    Create stringManuplation.js

---
+++ stringManuplation.js
@@ -0,0 +1,12 @@
+let product = " wireless headphones PRO ";
+
+let cleaned = product.trim().toLowerCase();
+
+cleaned = cleaned
+  .split(" ")
+  .map(w => w.charAt(0).toUpperCase() + w.slice(1))
+  .join(" ")
+  .replace("Pro", "Pro Edition");
+
+console.log(cleaned);
+console.log("Length:", cleaned.length);
