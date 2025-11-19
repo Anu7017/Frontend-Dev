ce17986549c44e5ea6ec2f120ec920508a1a6c83
anurag sharma <anuragsharma2092003@gmail.com>
2025-11-19 10:54:17 +0530

    Create products.js

---
+++ products.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<input id="productInput" placeholder="Add product">
+<button id="addBtn">Add</button>
+
+<ul id="productList"></ul>
+
+<script src="products.js"></script>
+</body>
+</html>
